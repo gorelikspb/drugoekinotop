517edf73d09bc17fb9b69930b478ad06182eb869
Your <you@example.com>
2020-07-11 22:00:33 +0300

    vk_api

--- vk_bot/bot_config.py
+++ vk_bot/bot_config.py
@@ -5,6 +5,6 @@ Configuration file for VK bot
 # token issued on the VK group web-administration page
 token = '0887065b582321c307a211c18641c89324b16dc56ef7b6b6a86c3312d517ecf4b1fa56c2c0061689cabac'
 # confirmation token issued on the VK group web-administration page in "Callback API" section
-confirmation_token = 'c8520ce3'
+confirmation_token = 'f7f9331a'
 # secret key for cross site request forgery protection. It will be in each VK server request
 secret_key = '88tommyleejones'
