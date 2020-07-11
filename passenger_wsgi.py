efe652877e9cf75d07911d70d590c8882e896bcb
Your <you@example.com>
2020-07-11 20:09:03 +0300

    path

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os, sys
 
-sys.path.insert(0, '/home/d/drimspb/drugoekino.top/public_html')
+sys.path.insert(0, '/home/d/drimspb/drugoekino.top/public_html/mysite')
 sys.path.insert(1, '/home/d/drimspb/.local/lib/python3.6/site-packages')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 from django.core.wsgi import get_wsgi_application
